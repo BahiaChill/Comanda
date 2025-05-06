0de1b4f3deff3373413ef1d1255965f07b121432
BahiaChill <bahiachillatencion@gmail.com>
2025-05-06 17:34:46 -0500

    users.js

--- Comanda Bahia Chill/src/mock/users.js
+++ Comanda Bahia Chill/src/mock/users.js
@@ -7,43 +7,56 @@ const users = [
 ];
 
 const products = [
-  { name: "Aguila", price: 3500, category: "cerveza" },
-  { name: "Poker", price: 3500, category: "cerveza" },
-  { name: "Budwaiser", price: 3000, category: "cerveza" },
-  { name: "Costena", price: 3000, category: "cerveza" },
-  { name: "Coronita", price: 4000, category: "cerveza" },
-  { name: "Corona", price: 8000, category: "cerveza" },
-  { name: "Stella Artois", price: 8000, category: "cerveza" },
-  { name: "1/2 Lider", price: 45000, category: "licor" },
-  { name: "1 Lider", price: 85000, category: "licor" },
-  { name: "1/2 Ron", price: 4000, category: "licor" },
-  { name: "1 Ron", price: 80000, category: "licor" },
-  { name: "Buchanan's", price: 220000, category: "licor" },
-  { name: "Onix Negro", price: 80000, category: "licor" },
-  { name: "Bacardi", price: 80000, category: "licor" },
-  { name: "Onix Amarillo", price: 90000, category: "licor" },
-  { name: "Cigarrillo", price: 1000, category: "otros" },
-  { name: "Michelada", price: 2000, category: "coctel" },
-  { name: "Agua", price: 3000, category: "bebida" },
-  { name: "Cafe", price: 3000, category: "bebida" },
-  { name: "Mani", price: 3000, category: "snack" },
-  { name: "Coca Cola", price: 4000, category: "bebida" },
-  { name: "Soda", price: 4000, category: "bebida" },
-  { name: "Sprite", price: 4000, category: "bebida" },
-  { name: "Ron con Cola", price: 28000, category: "coctel" },
-  { name: "Pina Chill", price: 28000, category: "coctel" },
-  { name: "Mojito", price: 28000, category: "coctel" },
-  { name: "Vodka Martini", price: 40000, category: "coctel" },
-  { name: "Pescera", price: 40000, category: "coctel" },
-  { name: "Fuego de Bahia", price: 12000, category: "coctel" },
-  { name: "Extasis Tropical", price: 12000, category: "coctel" },
-  { name: "Seduccion Tropical", price: 12000, category: "coctel" },
-  { name: "Mar de pasion", price: 12000, category: "coctel" },
-  { name: "Atardecer Tropical", price: 12000, category: "coctel" },
-  { name: "Naufragio", price: 12000, category: "coctel" },
-  { name: "Brisa Acida", price: 12000, category: "coctel" },
-  { name: "Tormenta Azul", price: 12000, category: "coctel" },
-  { name: "Rayo Caribeno", price: 12000, category: "coctel" }
+  // Cervezas
+  { id: 1, name: "Aguila", price: 3500, category: "cerveza" },
+  { id: 2, name: "Poker", price: 3500, category: "cerveza" },
+  { id: 3, name: "Budweiser", price: 3000, category: "cerveza" }, 
+  { id: 4, name: "Costeña", price: 3000, category: "cerveza" }, 
+  { id: 5, name: "Coronita", price: 4000, category: "cerveza" },
+  { id: 6, name: "Corona", price: 8000, category: "cerveza" },
+  { id: 7, name: "Stella Artois", price: 8000, category: "cerveza" },
+
+  // Licores
+  { id: 8, name: "1/2 Lider", price: 45000, category: "licor" }, 
+  { id: 9, name: "1 Lider", price: 85000, category: "licor" },
+  { id: 10, name: "1/2 Ron", price: 40000, category: "licor" }, 
+  { id: 11, name: "1 Ron", price: 80000, category: "licor" },
+  { id: 12, name: "Buchanan's", price: 220000, category: "licor" },
+  { id: 13, name: "Onix Negro", price: 80000, category: "licor" },
+  { id: 14, name: "Bacardi", price: 80000, category: "licor" },
+  { id: 15, name: "Onix Amarillo", price: 90000, category: "licor" },
+
+  // Otros
+  { id: 16, name: "Cigarrillo", price: 1000, category: "otros" },
+  
+  // Bebidas
+  { id: 17, name: "Agua", price: 3000, category: "bebida" },
+  { id: 18, name: "Cafe", price: 3000, category: "bebida" }, 
+  { id: 19, name: "Coca Cola", price: 4000, category: "bebida" },
+  { id: 20, name: "Soda", price: 4000, category: "bebida" },
+  { id: 21, name: "Sprite", price: 4000, category: "bebida" },
+
+  // Cocteles
+  { id: 22, name: "Michelada", price: 2000, category: "coctel" },
+  { id: 23, name: "Ron con Cola", price: 28000, category: "coctel" },
+  { id: 24, name: "Piña Chill", price: 28000, category: "coctel" }, 
+  { id: 25, name: "Mojito", price: 28000, category: "coctel" },
+  { id: 26, name: "Vodka Martini", price: 40000, category: "coctel" },
+  { id: 27, name: "Pescera", price: 40000, category: "coctel" },
+  
+  // Shots
+  { id: 28, name: "Fuego de Bahia", price: 12000, category: "coctel" },
+  { id: 29, name: "Extasis Tropical", price: 12000, category: "coctel" },
+  { id: 30, name: "Seduccion Tropical", price: 12000, category: "coctel" }, 
+  { id: 31, name: "Mar de Pasion", price: 12000, category: "coctel" }, 
+  { id: 32, name: "Atardecer Tropical", price: 12000, category: "coctel" },
+  { id: 33, name: "Naufragio", price: 12000, category: "coctel" },
+  { id: 34, name: "Brisa Acida", price: 12000, category: "coctel" }, 
+  { id: 35, name: "Tormenta Azul", price: 12000, category: "coctel" },
+  { id: 36, name: "Rayo Caribeño", price: 12000, category: "coctel" },
+
+  // Snacks
+  { id: 37, name: "Mani", price: 3000, category: "snack" } 
 ];
 
 export { users, products };
